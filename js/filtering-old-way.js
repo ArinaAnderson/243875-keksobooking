36598c2313de52eee0a9d243a72f5d6b42a13399
Marina <iolsoOfuolso@gmail.com>
2018-10-10 22:05:29 -0700

    работа над фильтрами

---
+++ js/filtering-old-way.js
@@ -0,0 +1,66 @@
+'use strict';
+(function () {
+  var selectType = document.querySelector('#housing-type');
+  var selectRooms = document.querySelector('#housing-rooms');
+  var selectGuests = document.querySelector('#housing-guests');
+  var selectPrice = document.querySelector('#housing-price');
+  var features = [];
+
+  // для функции filterTypeRoomGuest(pin, field), которая заменяет три старые filterType(pin),
+  // filterRoom(pin) и filterGuest(pin)
+  var fieldToPinProperty = {
+    'housing-type': 'type',
+    'housing-rooms': 'rooms',
+    'housing-guests': 'guests'
+  };
+
+  function filterTypeRoomGuest(pin, field) {
+    return field.value !== 'any' ? pin.offer[fieldToPinProperty[field.id]].toString() === field.value : true;
+  }
+
+  var priceValueToPriceRange = {
+    'low': function (pin) {
+      return pin.offer.price < 10000;
+    },
+    'middle': function (pin) {
+      return pin.offer.price >= 10000 && pin.offer.price <= 50000;
+    },
+    'high': function (pin) {
+      return pin.offer.price > 50000;
+    },
+    'any': function () {
+      return true;
+    }
+  };
+
+  function filterFeatures(pin) {
+    for (var i = 0; i < features.length; i++) {
+      if (pin.offer.features.indexOf(features[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  window.filtering = {
+    filterPins: function (loadedData) {
+      return loadedData.filter(function (pin) {
+        return filterTypeRoomGuest(pin, selectType) && priceValueToPriceRange[selectPrice.value](pin) && filterTypeRoomGuest(pin, selectRooms)
+          && filterTypeRoomGuest(pin, selectGuests) && filterFeatures(pin);
+      });
+    },
+
+    filterSelectChangeHandler: window.utils.debounce(function (evt, loadedPins) {
+      window.pin.update(loadedPins);
+    }),
+
+    filterFeatureChangeHandler: window.utils.debounce(function (evt, loadedPins) {
+      if (evt.target.checked) {
+        features.push(evt.target.value);
+      } else {
+        features.splice(features.indexOf(evt.target.value), 1);
+      }
+      window.pin.update(loadedPins);
+    })
+  };
+})();
--- js/filtering.js
+++ js/filtering.js
@@ -1,10 +1,6 @@
 'use strict';
 (function () {
-  // var selectFilters = document.querySelectorAll('.map__filter');
-  var selectType = document.querySelector('#housing-type');
-  var selectRooms = document.querySelector('#housing-rooms');
-  var selectGuests = document.querySelector('#housing-guests');
-  var selectPrice = document.querySelector('#housing-price');
+  var selectFilters = document.querySelectorAll('.map__filter');
   var features = [];
 
   // для функции filterTypeRoomGuest(pin, field), которая заменяет три старые filterType(pin),
@@ -15,8 +11,20 @@
     'housing-guests': 'guests'
   };
 
+  // при переборе массива полей с value НЕ 'any' вызывать соответствующие функции:
+  var fieldToFunction = {
+    'housing-type': filterTypeRoomGuest,
+    'housing-rooms': filterTypeRoomGuest,
+    'housing-guests': filterTypeRoomGuest,
+    'housing-price': priceValueToPriceRange
+  };
+
+  /* заменить три старых фильтра по типу, комнатам и гостям одной функцией, просто передавая поле не получается, потому что в первом случае
+  текст сравнивается с текстом, а в других - с числами, значит, чтобы привести эти три функции к одной я должна использовать
+  один формат - я выбрала toString(): там, где у пинов числа, я конвертирую в текст. Но тогда мы имеем излишнюю конвертацию текста в текст
+  в pin.offer.type. Еще эти три функции различаются свойствами пинов,  по которым идет сравнение с полем, поэтому мне понадобился объект fieldToPinProperty */
   function filterTypeRoomGuest(pin, field) {
-    return field.value !== 'any' ? pin.offer[fieldToPinProperty[field.id]].toString() === field.value : true;
+    return pin.offer[fieldToPinProperty[field.id]].toString() === field.value;
   }
 
   var priceValueToPriceRange = {
@@ -28,9 +36,6 @@
     },
     'high': function (pin) {
       return pin.offer.price > 50000;
-    },
-    'any': function () {
-      return true;
     }
   };
 
@@ -43,15 +48,25 @@
     return true;
   }
 
+  var notAnyFields;
+
   window.filtering = {
     filterPins: function (loadedData) {
       return loadedData.filter(function (pin) {
-        return filterTypeRoomGuest(pin, selectType) && priceValueToPriceRange[selectPrice.value](pin) && filterTypeRoomGuest(pin, selectRooms)
-          && filterTypeRoomGuest(pin, selectGuests) && filterFeatures(pin);
+        return notAnyFields.forEach(function (item) {
+          if (fieldToFunction[item.id] === filterTypeRoomGuest) {
+            return filterTypeRoomGuest(pin, item);
+          } else {
+            return priceValueToPriceRange[item.value](pin);
+          }
+        }) && filterFeatures(pin);
       });
     },
 
     filterSelectChangeHandler: window.utils.debounce(function (evt, loadedPins) {
+      notAnyFields = Array.prototype.filter.call(selectFilters, function (item) {
+        return item.value !== 'any';
+      });
       window.pin.update(loadedPins);
     }),
 
